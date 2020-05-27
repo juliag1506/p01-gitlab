1039ebf0d67ced948848bc79667c4e62202a6d00
juliag1506 <66021588+juliag1506@users.noreply.github.com>
2020-05-27 16:44:43 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -4,4 +4,6 @@ echo ("<br> Jestem programem 01");
 echo ("<br> Test 1222");
 echo ("<br> Test 2223");
 echo ("<br> Dalsza czesc programu");
+echo ("<br> Dalsza czesc programu 222 z GitHuba");
+echo ("<br> Julka");
 ?>
